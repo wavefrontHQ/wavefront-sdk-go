5008d12bb563babb1ba7338d27192025db3b7874
German Laullon Padilla <glaullon@vmware.com>
2019-01-18 22:38:20 +0100

    more typos

--- histogram/histogram.go
+++ histogram/histogram.go
@@ -138,12 +138,12 @@ func (h *histogramImpl) Sum() float64 {
 	h.mutex.Lock()
 	defer h.mutex.Unlock()
 
-	Sum := float64(0)
+	sun := float64(0)
 	h.currentTimedBin.tdigest.ForEachCentroid(func(mean float64, count uint32) bool {
-		Sum += mean * float64(count)
+		sun += mean * float64(count)
 		return true
 	})
-	return Sum
+	return sun
 }
 
 // Count returns the maen values of samples on this histogram.
