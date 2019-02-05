5e0e1d7d46ed0c3c182d73f7a12679cd13188e51
German Laullon <laullon@gmail.com>
2019-02-05 19:30:15 +0100

    Application Tags default values (#13)
    * Update application.go
    
    * Update application.go
    
    * Update application.go

--- application/application.go
+++ application/application.go
@@ -9,11 +9,13 @@ type Tags struct {
 	CustomTags  map[string]string
 }
 
-// Creates a new Tags with application and service name
+// New creates a new application Tags with application and service name
 func New(application, service string) Tags {
 	return Tags{
 		Application: application,
 		Service:     service,
+		Cluster:     "none",
+		Shard:       "none",
 		CustomTags:  make(map[string]string, 0),
 	}
 }
@@ -24,17 +26,12 @@ func (a Tags) Map() map[string]string {
 
 	allTags["application"] = a.Application
 	allTags["service"] = a.Service
-
-	if len(a.Cluster) > 0 {
-		allTags["cluster"] = a.Cluster
-	}
-
-	if len(a.Shard) > 0 {
-		allTags["shard"] = a.Shard
-	}
+	allTags["cluster"] = a.Cluster
+	allTags["shard"] = a.Shard
 
 	for k, v := range a.CustomTags {
 		allTags[k] = v
 	}
+
 	return allTags
 }
