cac21dd0fa2a53699388f8f922e3868d0cd03990
German Laullon Padilla <glaullon@vmware.com>
2019-11-12 14:27:41 +0100

    EventsPort and eventHandler

--- senders/configs.go
+++ senders/configs.go
@@ -39,6 +39,7 @@ type ProxyConfiguration struct {
 	MetricsPort      int // metrics port on which the proxy is listening on, typically 2878.
 	DistributionPort int // distribution port on which the proxy is listening on, typically 40000.
 	TracingPort      int // tracing port on which the proxy is listening on.
+	EventsPort       int // events port on which the proxy is listening on.
 
 	FlushIntervalSeconds int // defaults to 1 second
 }
