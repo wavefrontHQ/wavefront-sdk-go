150e4f3af5af47a41a046babee156377f04d3886
Luke Winikates <winikatesl@vmware.com>
2023-08-30 14:13:00 -0700

    docs: add an example of sending to a Wavefront proxy with custom ports

--- senders/example_newsender_proxy_test.go
+++ senders/example_newsender_proxy_test.go
@@ -6,8 +6,14 @@ import (
 
 func ExampleNewSender_proxy() {
 	// For Proxy endpoints, by default metrics and histograms are sent to
-	// port 2878 and spans are sent to port 30001.
+	// port 2878 and traces are sent to port 30001.
 	sender, err := wavefront.NewSender("http://localhost")
+
+	// To use non-default ports, specify the metric/histogram port in the url,
+	// and set the port for traces using the TracesPort Option.
+	sender, err = wavefront.NewSender("https://localhost:4443",
+		wavefront.TracesPort(55555))
+
 	if err != nil {
 		// handle error
 	}
